52dbd1b5edc181e490f626487c2892b24fc5032b
Srujan Naik <srujannaik214@gmail.com>
2026-03-22 00:51:07 +0530

    Implement JIT performance comparison in JITDemo
    Added JIT demonstration to compare execution times.

--- src/JITDemo.java
+++ src/JITDemo.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
 public class JITDemo {
+
+    public static void main(String[] args) {
+
+        // First run
+        long start1 = System.currentTimeMillis();
+        runLoop();
+        long end1 = System.currentTimeMillis();
+
+        // Second run (should be faster due to JIT)
+        long start2 = System.currentTimeMillis();
+        runLoop();
+        long end2 = System.currentTimeMillis();
+
+        System.out.println("First Run Time: " + (end1 - start1) + " ms");
+        System.out.println("Second Run Time: " + (end2 - start2) + " ms");
+    }
+
+    public static void runLoop() {
+        for (int i = 0; i < 100000000; i++) {
+            int x = i * i;
+        }
+    }
 }
